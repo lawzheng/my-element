986b1451e0d5aa3197d4a0517b7d47d0b153f230
lawzheng <602285510@qq.com>
2022-06-19 19:45:02 +0800

    增加区域选择组件'

---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <m-choose-area @change="changeArea" />
+</template>
+
+<script setup lang='ts'>
+import mChooseArea from '../../components/chooseArea/src/index.vue'
+
+const changeArea = (val: any) => {
+  console.log(val);
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
