15c2a88f488622d1b24406dca420994a1e988d3d
lawzheng <602285510@qq.com>
2022-06-19 17:50:13 +0800

    add vueuse for copy

--- src/hooks/useCopy/index.ts
+++ src/hooks/useCopy/index.ts
@@ -1,11 +1,19 @@
 import { ElMessage } from 'element-plus'
+import { useClipboard } from '@vueuse/core'
+import { ref } from 'vue'
 
 export const useCopy = (text: string) => {
-  const input = document.createElement('input')
-  input.value = text
-  document.body.appendChild(input)
-  input.select()
-  document.execCommand('Copy')
-  document.body.removeChild(input)
-  ElMessage.success('复制成功')
+  // const input = document.createElement('input')
+  // input.value = text
+  // document.body.appendChild(input)
+  // input.select()
+  // document.execCommand('Copy')
+  // document.body.removeChild(input)
+  // ElMessage.success('复制成功')
+  const source = ref(text)
+  const { copy, copied } = useClipboard({ source })
+  copy()
+  if (copied) {
+    ElMessage.success('复制成功')
+  }
 }
